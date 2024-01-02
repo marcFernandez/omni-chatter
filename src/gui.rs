32c37de37d7fca5ce9ecf7090b149133980416f6
marcFernandez <markiff@gmail.com>
2024-01-01 17:48:08 -0800

    feat(toast): Add level to toasts

--- src/gui.rs
+++ src/gui.rs
@@ -1,29 +1,51 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::{
+    collections::HashSet,
     sync::{Arc, Mutex},
-    time::{Duration, SystemTime}, collections::HashSet,
+    time::{Duration, SystemTime},
 };
 
 use eframe::egui;
-use egui::Separator;
+use egui::{Color32, Label, RichText, Separator};
 
 use crate::command::{BotCommand, CommandHandler};
+
+#[derive(Clone)]
+enum ToastLevel {
+    Info,
+    Warn,
+    Error,
+    Success,
+}
 
 #[derive(Clone)]
 struct Toast {
     start: SystemTime,
     duration: Duration,
     msg: String,
+    level: ToastLevel,
 }
 
 impl Toast {
-    fn new(duration: Duration, msg: String) -> Self {
+    fn new(duration: Duration, msg: String, level: ToastLevel) -> Self {
         Self {
             start: SystemTime::now(),
             duration,
             msg,
+            level,
         }
+    }
+
+    fn get_label(&self) -> Label {
+        let text = RichText::new(&self.msg);
+        let text = match self.level {
+            ToastLevel::Info => text,
+            ToastLevel::Warn => text.color(Color32::LIGHT_YELLOW),
+            ToastLevel::Error => text.color(Color32::LIGHT_RED).strong(),
+            ToastLevel::Success => text.color(Color32::LIGHT_GREEN),
+        };
+        Label::new(text)
     }
 }
 
@@ -38,11 +60,13 @@ struct OmniChatter {
     current_command: BotCommand,
     toasts: Vec<Toast>,
     state: State,
+    command_search: String,
 }
 
 impl OmniChatter {
     fn new(command_handler: Arc<Mutex<CommandHandler>>) -> Self {
         Self {
+            command_search: "".to_string(),
             command_handler,
             current_command: BotCommand {
                 name: String::new(),
@@ -82,23 +106,29 @@ impl eframe::App for OmniChatter {
         //  _________________________________
         // |  Omnichatter                    |
         // |---------------------------------|
-        // |             | Command*          |
-        // |  Command1*  |                   |
-        // |  Command2   | Command_contents  |
-        // |  ...        |-------------------|
-        // |             | Info area         |
-        // |             |                   |
-        // |             |                   |
+        // |  Command1*  | Command*          |
+        // |  Command2   |                   |
+        // |  ...        | Command_contents  |
+        // |-------------|-------------------|
+        // |  Action1    | Info area         |
+        // |  Action2    |                   |
+        // |  ...        |                   |
         // |             |                   |
         //  ---------------------------------
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            ui.heading("Omnichatter");
+            ui.spacing_mut().item_spacing.y = 20.0;
+            ui.label(RichText::new("Omnichatter").heading().strong())
         });
-        egui::SidePanel::left("left_panel").show(ctx, |ui| {
+        egui::SidePanel::left("left_panel").resizable(false).show(ctx, |ui| {
+            ui.spacing_mut().item_spacing.y = 5.0;
+            ui.text_edit_singleline(&mut self.command_search);
             for command_name in command_names {
-                if ui.button(&*command_name).clicked() {
-                    self.current_command = command_handler.get_command(&command_name);
-                    self.state = State::DisplayCommand;
+                if self.command_search.len() == 0 || command_name.starts_with(&self.command_search) {
+                    if ui.button(&*command_name).clicked() {
+                        self.command_search = String::new();
+                        self.current_command = command_handler.get_command(&command_name);
+                        self.state = State::DisplayCommand;
+                    };
                 };
             }
             ui.add(Separator::default().horizontal());
@@ -111,34 +141,63 @@ impl eframe::App for OmniChatter {
             };
         });
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.spacing_mut().item_spacing.x = 10.0;
+            ui.spacing_mut().item_spacing.y = 10.0;
             ui.scope(|ui| match self.state {
                 State::DisplayCommand => {
                     ui.strong(&self.current_command.name);
                     ui.text_edit_multiline(&mut self.current_command.contents);
-                    if ui.button("Update").clicked() {
-                        command_handler
-                            .update_command_content(&self.current_command.name, &self.current_command.contents)
-                            .expect("Command to exist");
-                        self.toasts.push(Toast::new(
-                            Duration::from_secs_f32(1.25),
-                            format!("Command {} updated successfully", self.current_command.name),
-                        ))
-                    }
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                        if ui.button("Update").clicked() {
+                            command_handler
+                                .update_command_content(&self.current_command.name, &self.current_command.contents)
+                                .expect("Command to exist");
+                            self.toasts.push(Toast::new(
+                                Duration::from_secs_f32(1.25),
+                                format!("Command {} updated successfully", self.current_command.name),
+                                ToastLevel::Success
+                            ))
+                        }
+                        if ui
+                            .add(egui::Button::new("Delete").fill(Color32::from_rgb(94, 25, 25)))
+                            .clicked()
+                        {
+                            command_handler
+                                .delete_command(&self.current_command.name)
+                                .expect("Command to be deleted");
+                            self.toasts.push(Toast::new(
+                                Duration::from_secs_f32(1.25),
+                                format!("Command {} deleted successfully", self.current_command.name),
+                                ToastLevel::Success
+                            ));
+                            self.state = State::Idle;
+                        }
+                    });
                 }
                 State::CreateCommand => {
                     ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
                         ui.label("name:");
                         ui.text_edit_singleline(&mut self.current_command.name);
                     });
-                    ui.label("contents:");
-                    ui.text_edit_multiline(&mut self.current_command.contents);
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                        ui.label("contents:");
+                        ui.text_edit_multiline(&mut self.current_command.contents);
+                    });
                     if ui.button("Create").clicked() {
                         match command_handler.create_command(&self.current_command) {
-                            Ok(_) => self.toasts.push(Toast::new(
-                                Duration::from_secs_f32(1.5),
-                                format!("Command {} created successfully!", &self.current_command.name),
+                            Ok(_) => {
+                                self.toasts.push(Toast::new(
+                                    Duration::from_secs_f32(2.5),
+                                    format!("Command {} created successfully!", &self.current_command.name),
+                                    ToastLevel::Success
+                                ));
+                                self.state = State::DisplayCommand
+                            }
+                            Err(err) => self.toasts.push(Toast::new(
+                                Duration::from_secs_f32(2.5),
+                                format!("Error creating command: {:?}", err),
+                                ToastLevel::Error
                             )),
-                            Err(err) => self.toasts.push(Toast::new(Duration::from_secs_f32(2.5), format!("Error creating command: {:?}", err))),
                         }
                     }
                 }
@@ -146,13 +205,12 @@ impl eframe::App for OmniChatter {
                     ui.label("Select any command or action");
                 }
             });
-            ui.add(Separator::default().horizontal());
             let now = SystemTime::now();
             // TODO: find a better approach without re-creating memory
             let mut toasts: Vec<Toast> = Vec::new();
             for toast in &self.toasts {
                 if now.duration_since(toast.start).unwrap() < toast.duration {
-                    ui.label(&toast.msg);
+                    ui.add(toast.get_label());
                     toasts.push(toast.clone());
                 }
             }
